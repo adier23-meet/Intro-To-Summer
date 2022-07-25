56c533a8b10fc45ad7ed2affa02cff39547d45ca
adier23-meet <109903931+adier23-meet@users.noreply.github.com>
2022-07-25 09:51:56 +0300

    commit

---
+++ pythonReview.py
@@ -0,0 +1,11 @@
+def creat_youtube_channel(title, description):
+	dict1 = {"title": title,
+	"description": description, 
+	"likes": 0, 
+	"dislikes": 0, 
+	"comments": {}}
+def like (dict1):
+	if "likes" in dict1:
+		dict1["likes"] =+ 1
+	def return(dict1):
+		return dict1
